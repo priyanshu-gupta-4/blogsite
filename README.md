5b6db1a15f478884f4d1734dc27e9eab1ae1c073
GUPTA-JI <guptapriyanshu2001@gmail.com>
2020-03-30 04:24:36 +0530

    added a database and delete page

---
+++ README.md
@@ -0,0 +1,1 @@
+# blog it is project of a web app development with full stack development "blog application website"
--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash=require("lodash");
+const mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://username:password@cluster0-pbvgw.mongodb.net/blogdb",{useNewUrlParser:true , useUnifiedTopology:true});
 //const trunk=require("C:/Users/pg/Desktop/webd2019/webdevelopment/blog/truncate.js");
-const posts=[];
+const postSchema=new mongoose.Schema({
+  topic: String,
+  postBody:String
+});
+
+const Post=mongoose.model("Post",postSchema);
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -18,7 +25,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home",{content:homeStartingContent,posts:posts});
+  Post.find(function(err,posta){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.render("home",{content:homeStartingContent,posts:posta});
+      }
+  });
 });
 app.get("/contact",function(req,res){
   res.render("contact",{content:contactContent});
@@ -30,36 +44,41 @@ app.get("/about",function(req,res){
   res.render("about",{content:aboutContent});
 });
 app.get("/posts/:topic",function(req,res){
-posts.forEach(function(post){
-  if (lodash.lowerCase(post.title)===lodash.lowerCase(req.params.topic))
-  {
-    res.render("posts",{post:post});
+  const topice=lodash.lowerCase(req.params.topic);
+  Post.findOne({topic:req.params.topic},function(err,postget){
+    if(postget){
+    res.render("posts",{post:postget});
   }
   else{
-    console.log("not found");
+    res.redirect("/");
   }
+  });
 });
+app.get("/delete",function(req,res){
+  Post.find(function(err,postget){
+    res.render("delete",{posts:postget});
+  });
 });
-
-app.post("/",function(req,res){
-  const post={
-    title:req.body.postTitle,
-    body:req.body.postBody
-  };
-  posts.push(post);
+app.post("/compose",function(req,res){
+  const newPost=new Post({
+    topic:req.body.postTitle,
+    postBody:req.body.postBody
+  });
+  newPost.save();
   res.redirect("/");
 });
-
-
-
-
-
-
-
-
-
-
-
+app.post("/delete",function(req,res){
+  id=req.body.posttopic;
+  Post.findByIdAndDelete(id,function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("success");
+      res.redirect("/");
+    }
+  });
+});
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
